98509e0d0ffde92d8c7c7dbcb6e4d6913bd2a205
shahmadseif-jpg <shahmadseif@gmail.com>
2026-05-13 01:33:50 -0500

    Update page.tsx

--- app/[locale]/articles/page.tsx
+++ app/[locale]/articles/page.tsx
@@ -1,5 +1,6 @@
 import { setRequestLocale, getTranslations } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
+import ArticlesList from '@/components/ArticlesList';
 
 export async function generateMetadata({
   params,
@@ -36,15 +37,7 @@ function ArticlesContent() {
           </p>
         </div>
 
-        <div className="max-w-2xl mx-auto bg-navy-50/40 border border-navy-100 rounded-lg p-12 text-center">
-          <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" className="mx-auto mb-4 text-navy-400">
-            <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z" />
-            <polyline points="14 2 14 8 20 8" />
-            <line x1="16" y1="13" x2="8" y2="13" />
-            <line x1="16" y1="17" x2="8" y2="17" />
-          </svg>
-          <p className="text-navy-600">{t('no_articles')}</p>
-        </div>
+        <ArticlesList />
       </div>
     </div>
   );
